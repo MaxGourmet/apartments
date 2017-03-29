4b815548618f00a9bc8d877c6d8b076637ce2ae0
Dmitriy Pastukhov <eryth.reborned@gmail.com>
2017-03-29 10:25:04 +0300

    apartments Dima

--- application/controllers/Apartments.php
+++ application/controllers/Apartments.php
@@ -11,7 +11,6 @@ class Apartments extends MY_Controller
 
     public function index()
     {
-      var_dump('yo');exit;
         $this->title = $this->configs->get(false, 'apartments_title');
         $apartments = $this->apartments->get();
         $this->showView(
