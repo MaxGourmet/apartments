ebc28ddb0c7b7bf205e67c99e3b25a7865bcd3ec
Dmitriy Pastukhov <eryth.reborned@gmail.com>
2017-03-29 10:34:12 +0300

    apartments Dima

--- application/views/layout.php
+++ application/views/layout.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="utf-8">
     <title><?= $title ?></title>
-    <link type="text/css" rel="stylesheet" href="/assets/css/style.css" />
     <link type="text/css" rel="stylesheet" href="/assets/css/jquery-ui-1.9.2.custom.min.css" />
+    <link type="text/css" rel="stylesheet" href="/assets/css/style.css" />
     <script type="application/javascript" src="/assets/js/jquery-1.8.3.js"></script>
     <script type="application/javascript" src="/assets/js/jquery-ui-1.9.2.custom.min.js"></script>
     <script type="application/javascript" src="/assets/js/script.js"></script>
