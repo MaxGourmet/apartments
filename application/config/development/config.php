37c88ba40ea4384404d6bb512764032fd4b7a580
MaxGourmet <keyfay@ukr.net>
2017-03-29 23:34:51 +0300

    apartments

--- application/config/development/config.php
+++ application/config/development/config.php
@@ -3,4 +3,3 @@ include dirname(__FILE__).'/../'.basename(__FILE__);
 $config['admin'] = 'ZVQRfoe7C8';
 $config['dev'] = 'FnSXrImUSr';
 $config['base_url'] = 'http://apartments';
-$config['sess_save_path'] = 'C:\WebServers\home\apartments\www\application\sessions';
