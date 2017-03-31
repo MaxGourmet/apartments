47185adb28400e34a6f9702ac1db03ff54fa25c3
Dmitriy Pastukhov <eryth.reborned@gmail.com>
2017-03-31 09:02:33 +0300

    apartments Dima

--- application/views/layout.php
+++ application/views/layout.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
     <title><?= $title ?></title>
     <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
     <link type="text/css" rel="stylesheet" href="/assets/css/jquery-ui-1.9.2.custom.min.css" />
