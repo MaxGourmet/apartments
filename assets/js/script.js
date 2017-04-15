464111d0c76bc1e3a1abd7ed2b6a2f97d13f90f5
MaxGourmet <keyfay@ukr.net>
2017-04-15 14:37:33 +0300

    apartments

--- assets/js/script.js
+++ assets/js/script.js
@@ -174,55 +174,55 @@ $(function() {
 
 
 
-    //function detectswipe(el,func) {
-    //    swipe_det = new Object();
-    //    swipe_det.sX = 0;
-    //    swipe_det.sY = 0;
-    //    swipe_det.eX = 0;
-    //    swipe_det.eY = 0;
-    //    var min_x = 20;  //min x swipe for horizontal swipe
-    //    var max_x = 40;  //max x difference for vertical swipe
-    //    var min_y = 40;  //min y swipe for vertical swipe
-    //    var max_y = 50;  //max y difference for horizontal swipe
-    //    var direc = "";
-    //    var ele = document.getElementById(el);
-    //    ele.addEventListener('touchstart',function(e){
-    //        var t = e.touches[0];
-    //        swipe_det.sX = t.screenX;
-    //        swipe_det.sY = t.screenY;
-    //    },false);
-    //    //ele.addEventListener('touchmove',function(e){
-    //    //    // e.preventDefault();
-    //    //    var t = e.touches[0];
-    //    //    swipe_det.eX = t.screenX;
-    //    //    swipe_det.eY = t.screenY;
-    //    //},false);
-    //    ele.addEventListener('touchend',function(e){
-    //        //horizontal detection
-    //        if ((((swipe_det.eX - min_x > swipe_det.sX) || (swipe_det.eX + min_x < swipe_det.sX)) && ((swipe_det.eY < swipe_det.sY + max_y) && (swipe_det.sY > swipe_det.eY - max_y)))) {
-    //            if(swipe_det.eX > swipe_det.sX) direc = "r";
-    //            else direc = "l";
-    //        }
-    //        //vertical detection
-    //        if ((((swipe_det.eY - min_y > swipe_det.sY) || (swipe_det.eY + min_y < swipe_det.sY)) && ((swipe_det.eX < swipe_det.sX + max_x) && (swipe_det.sX > swipe_det.eX - max_x)))) {
-    //            if(swipe_det.eY > swipe_det.sY) direc = "d";
-    //            else direc = "u";
-    //        }
-    //
-    //        if (direc != "") {
-    //            if(typeof func == 'function') func(el,direc);
-    //        }
-    //        direc = "";
-    //    },false);
-    //}
-    //
-    //detectswipe('body',function (el,d) {
-    //    if(d === 'l'){
-    //        $('#next_month').click();
-    //    } else if(d === 'r'){
-    //        $('#prev_month').click();
-    //    }
-    //});
+    function detectswipe(el,func) {
+        swipe_det = new Object();
+        swipe_det.sX = 0;
+        swipe_det.sY = 0;
+        swipe_det.eX = 0;
+        swipe_det.eY = 0;
+        var min_x = 20;  //min x swipe for horizontal swipe
+        var max_x = 40;  //max x difference for vertical swipe
+        var min_y = 40;  //min y swipe for vertical swipe
+        var max_y = 50;  //max y difference for horizontal swipe
+        var direc = "";
+        var ele = document.getElementById(el);
+        ele.addEventListener('touchstart',function(e){
+            var t = e.touches[0];
+            swipe_det.sX = t.screenX;
+            swipe_det.sY = t.screenY;
+        },false);
+        ele.addEventListener('touchmove',function(e){
+            // e.preventDefault();
+            var t = e.touches[0];
+            swipe_det.eX = t.screenX;
+            swipe_det.eY = t.screenY;
+        },false);
+        ele.addEventListener('touchend',function(e){
+            //horizontal detection
+            if ((((swipe_det.eX - min_x > swipe_det.sX) || (swipe_det.eX + min_x < swipe_det.sX)) && ((swipe_det.eY < swipe_det.sY + max_y) && (swipe_det.sY > swipe_det.eY - max_y)))) {
+                if(swipe_det.eX > swipe_det.sX) direc = "r";
+                else direc = "l";
+            }
+            //vertical detection
+            if ((((swipe_det.eY - min_y > swipe_det.sY) || (swipe_det.eY + min_y < swipe_det.sY)) && ((swipe_det.eX < swipe_det.sX + max_x) && (swipe_det.sX > swipe_det.eX - max_x)))) {
+                if(swipe_det.eY > swipe_det.sY) direc = "d";
+                else direc = "u";
+            }
+
+            if (direc != "") {
+                if(typeof func == 'function') func(el,direc);
+            }
+            direc = "";
+        },false);
+    }
+
+    detectswipe('body',function (el,d) {
+        if(d === 'l'){
+            $('#next_month').click();
+        } else if(d === 'r'){
+            $('#prev_month').click();
+        }
+    });
 
     var calendarTable = $('table.calendar');
     if ($(calendarTable).length) {
@@ -273,40 +273,3 @@ $(function() {
         }
     }
 });
-var touchstartX = 0;
-var touchstartY = 0;
-var touchendX = 0;
-var touchendY = 0;
-
-var gesuredZone = document;
-
-gesuredZone.addEventListener('touchstart', function(event) {
-    touchstartX = event.screenX;
-    touchstartY = event.screenY;
-}, false);
-
-gesuredZone.addEventListener('touchend', function(event) {
-    touchendX = event.screenX;
-    touchendY = event.screenY;
-    handleGesure();
-}, false);
-
-function handleGesure() {
-    var swiped = 'swiped: ';
-    console.log(touchstartX, touchendX);
-    if (touchendX < touchstartX) {
-        alert(swiped + 'left!');
-    }
-    if (touchendX > touchstartX) {
-        alert(swiped + 'right!');
-    }
-    if (touchendY < touchstartY) {
-        alert(swiped + 'down!');
-    }
-    if (touchendY > touchstartY) {
-        alert(swiped + 'left!');
-    }
-    if (touchendY == touchstartY) {
-        alert('tap!');
-    }
-}
