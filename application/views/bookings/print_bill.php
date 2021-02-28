572abdd345841497c3468f9cd6fdc0ec6d6b86ca
gourmet <keyfay@ukr.net>
2021-03-01 01:05:40 +0200

    add customers

--- application/views/bookings/print_bill.php
+++ application/views/bookings/print_bill.php
@@ -2,11 +2,21 @@
 	<div style="background-color: #0000FF;width: 100%">
 		PRINTER
 	</div>
+	<table>
+		<tr>
+			<td>1</td>
+			<td>2</td>
+			<td>3</td>
+		</tr>
+	</table>
 </div>
 <a id="printer" title="Print">Print</a>
 <script>
 	$(document).on('click', '#printer', function() {
 		$(this).hide();
+		$('.menu, #menu-trigger-label, .search, h1').hide();
 		window.print();
+		$(this).show();
+		$('.menu, #menu-trigger-label, .search, h1').show();
 	});
 </script>
