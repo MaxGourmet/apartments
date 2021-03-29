9483a77b9db4be943bf956ed867e51b1aa3ff7ac
gourmet <keyfay@ukr.net>
2021-03-30 01:02:56 +0300

    add services

--- index.php
+++ index.php
@@ -73,11 +73,11 @@ if (strripos($_SERVER['SERVER_NAME'], 'apartments')) {
 switch (ENVIRONMENT)
 {
 	case 'development':
+	case 'testing':
 		error_reporting(-1);
 		ini_set('display_errors', 1);
 	break;
 
-	case 'testing':
 	case 'production':
 		ini_set('display_errors', 0);
 		if (version_compare(PHP_VERSION, '5.3', '>='))
