67d4e8e4a1f0cf01719b5cb517e6ac716c6bb26c
gourmet <keyfay@ukr.net>
2021-04-19 00:36:25 +0300

    add services

--- application/views/bookings/print_bill.php
+++ application/views/bookings/print_bill.php
@@ -13,18 +13,23 @@
 		</div>
 		<br />
 		<div style="font-size: 0.45rem;"><pre><?= $billConfigs['tax_numbers']; ?></pre></div>
+	</div>
+	<div style="text-align: left">
+		<div style="font-weight: bold;"><pre><?= $billConfigs['tax_address']; ?></pre></div>
+	</div>
+	<div style="text-align: right">
 		<div>
 			Düsseldorf, <?= date('d.m.Y'); ?>
 		</div>
 	</div>
-	<h1>Rechnung: <?= $booking['id'] . "-" . date('dmY'); ?></h1>
-	<table>
-		<tr>
-			<td style="border: 1px solid ">1</td>
-			<td style="border: 1px solid ">2</td>
-			<td style="border: 1px solid ">3</td>
-		</tr>
-	</table>
+	<div>
+		<div style="font-size: 1rem; font-weight: bold">
+			Rechnung: <?= $booking['id'] . "-" . date('dmY'); ?>
+		</div>
+		<div style="font-weight: bold">
+			Hiermit berechnen wir für unsere Leistungen:
+		</div>
+	</div>
 </div>
 <a id="printer" title="Print">Print</a>
 <script>
